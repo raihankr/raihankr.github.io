a4c9cdfa5fac3c9b77ba97dbdc69f63b4d66760f
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2025-02-16 13:55:04 +0700

    Install PostCSS, Pug, Express, and Autoprefixer

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
