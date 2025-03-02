11b40b2d64896bcb7b6a2cc344099f0b5beb6e94
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2025-03-02 18:00:46 +0700

    Add content interpolation for skills

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const app = require('./app.js');
+const contents = require('./utils/contents.js');
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+app.locals.contents = contents;
 
-const content = {
-  'skills': fs.readFileSync('./contents/skills.json'),
-};
-
-// Use pug as the template engine for directory `views`
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// Serve static files from the `public` directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Routes
-app.get('/', (req, res) => {
-  res.render('index', content.skills);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-// Start the server
-// const server = app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-// Watch the contents directory for changes
-const directoryPath = path.join(__dirname, 'contents');
-fs.watch(directoryPath, (eventType, filename) => {
-  if (filename) {
-    console.log(`${filename} file changed`);
-    var basename = path.basename(filename, '.json');
-    content[basename] = fs.readFileSync(`./contents/${filename}`);
-    console.log(content);
-  }
-});
-
-module.exports = app;
---
+++ utils/contents.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const contents = {
+  skills_overview: '',
+};
+
+const directoryPath = path.join(__dirname, '../contents');
+fs.watch(directoryPath, reloadContent);
+
+for (let filename in contents) {
+  reloadContent(null, `${filename}.json`);
+}
+
+function reloadContent(eventType, filename) {
+  if (filename) {
+    console.log(`Loading ${filename}`);
+    var basename = path.basename(filename, '.json');
+    contents[basename] = JSON.parse(fs.readFileSync(path.join(directoryPath, filename)));
+  }
+}
+
+
+module.exports = contents;
